fdcec7dc394a162d15d5272d1c83b161b2cc99e1
Taha Ghasemi <taha.ghasemi@gmail.com>
2013-08-06 13:06:51 +0430

    Add decode function

--- zal.extractor/src/main/java/ir/co/bayan/simorq/zal/extractor/model/Decode.java
+++ zal.extractor/src/main/java/ir/co/bayan/simorq/zal/extractor/model/Decode.java
@@ -2,58 +2,40 @@ package ir.co.bayan.simorq.zal.extractor.model;
 
 import ir.co.bayan.simorq.zal.extractor.evaluation.EvaluationContext;
 
+import java.net.URLDecoder;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.apache.commons.lang3.Validate;
 
 /**
- * Truncates a string if its size is greater than max.
+ * Decodes the given url string.
  * 
  * @author Taha Ghasemi <taha.ghasemi@gmail.com>
  * 
  */
 @XmlRootElement
-public class Truncate extends Function {
+public class Decode extends Function {
 
-	@XmlAttribute
-	private int max;
-
-	/**
-	 * @return the max
-	 */
-	public int getMax() {
-		return max;
-	}
-
-	/**
-	 * @param max
-	 *            the max to set
-	 */
-	public void setMax(int max) {
-		this.max = max;
-	}
+	private static final String UTF_8 = "UTF-8";
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<?> extract(Object root, EvaluationContext context) throws Exception {
-		Validate.isTrue(args != null && args.size() == 1, "Only one inner function is expected.");
+		Validate.isTrue(args != null && args.size() == 1, "Only one arg should be specified");
+
 		List<String> res = (List<String>) args.get(0).extract(root, context);
 		for (int i = 0; i < res.size(); i++) {
-			String item = res.get(i);
-			if (item != null && item.length() > max) {
-				item = item.substring(0, max);
-				res.set(i, item);
-			}
+			String url = res.get(i);
+			res.set(i, URLDecoder.decode(url, UTF_8));
 		}
 		return res;
 	}
 
 	@Override
 	public String toString() {
-		return "Truncate [max=" + max + "]";
+		return "Decode [" + super.toString() + "]";
 	}
 
 }

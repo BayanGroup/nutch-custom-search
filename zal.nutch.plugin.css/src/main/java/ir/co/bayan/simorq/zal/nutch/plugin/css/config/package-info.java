b93c36fa33da217d1a6f4d302573d08d95ef0b1a
Taha Ghasemi <taha.ghasemi@gmail.com>
2014-04-24 10:13:23 +0430

    Remove old css project

--- zal.nutch.plugin.css/src/main/java/ir/co/bayan/simorq/zal/nutch/plugin/css/config/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlSchema(namespace = "http://bayan.ir", elementFormDefault = XmlNsForm.QUALIFIED)
-package ir.co.bayan.simorq.zal.nutch.plugin.css.config;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
